7e4fc0314f279150db4bfca1bc1b3cb8e656abf7
bewe <93899302+bernarduswillson@users.noreply.github.com>
2023-10-30 09:11:35 +0700

    feat: add login and register page

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import SEO from '@src/components/SEO/SEO'
+import Register from '@src/contexts/Register'
 
 const RegisterPage: React.FC = () => {
   return (
     <SEO title="Toco for Lecturer | Register">
-      <div>
-        tes
-      </div>
+      <Register />
     </SEO>
   )
 }
