fd14fcac9fb267af74c242555760bd05fe3a698a
Ditra Amadia <ditrarizqaamadia@gmail.com>
2023-11-01 19:22:32 +0700

    feat: add auth validation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Cookie from 'js-cookie';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const token = Cookie.get('token');
+
+  // Token validation
+  useEffect(() => {
+    if (!token) {
+      navigate('/login');
+    }
+  }, [token, navigate]);
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello kontl</h1>
